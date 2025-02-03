b1c3929f9488eb28f1cb697baea3ff86ef4f85f7
Sumit Paul <sumit.18.paul@gmail.com>
2025-02-03 20:07:49 +0530

    feat: rename webhook route file and add custom 404 error page

--- api/routes/api_routes.go
+++ api/routes/api_routes.go
@@ -10,6 +10,7 @@ import (
 func ApiRoutes(router *mux.Router) {
 	router.HandleFunc("/schedule", SchduleHandler).Methods("POST")
 	router.HandleFunc("/webhook/verify", VerifyWebhookHandler).Methods("POST")
+	router.NotFoundHandler = http.HandlerFunc(NotFoundHandler)
 }
 
 func SchduleHandler(w http.ResponseWriter, r *http.Request) {
@@ -20,4 +21,8 @@ func SchduleHandler(w http.ResponseWriter, r *http.Request) {
 func VerifyWebhookHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	controllers.VerifyWebhookHandler(ctx, w, r)
+}
+
+func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "public/404.html")
 }
